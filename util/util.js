38e10ed15ba4f6de4734cc37f7ada7add4c6098e
Anson Foong <afoong@albany.edu>
2019-09-25 07:40:53 -0400

    Removed promisify function and used fsPromises instead to create the project files.

--- util/util.js
+++ util/util.js
@@ -1,9 +1,13 @@
-module.exports.promisify = function(fn, ...args)
-{
-    return function(...args)
-    {
-        return new Promise((resolve, reject) => {
-            fn(...args, (err) => !err ? resolve('done') : reject(err));
-        });
+const path = require('path');
+const fs = require('fs').promises;
+
+module.exports.generateProject = function(type, projectName, configObj) {
+    const CURRENT_DIR = process.cwd();
+    if(type === 'djs') {
+        fs.mkdir(path.join(CURRENT_DIR, projectName))
+        .then(() => fs.mkdir(path.join(CURRENT_DIR, projectName, 'config')))
+        .then(() => fs.copyFile(path.join(__dirname, '..', 'templates', 'bot.js'), path.join(CURRENT_DIR, projectName, 'bot.js')))
+        .then(() => fs.writeFile(path.join(CURRENT_DIR, projectName, 'config', 'config.json'), JSON.stringify(configObj)))
+        .then(() => console.log("Generated Discord.JS Project")).catch(err => console.log(err));
     }
 }
