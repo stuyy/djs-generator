99cec2e059b8fe9013a2f88f924fceeadea9fe44
Anson Foong <afoong@albany.edu>
2019-09-25 06:50:17 -0400

    Generate the project folder in the process.cwd aka current dir where user executed script.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const prompts = require('prompts');
+const fs = require('fs');
+const path = require('path');
+const utils = require('./util/util');
 
 (async () => {
+    console.log(process.cwd())
     const response = await prompts({
       type: 'text',
       name: 'lib',
@@ -13,6 +17,12 @@ const prompts = require('prompts');
         message: 'Enter the name of the file',
         validate: file => file.endsWith(".js") || file.endsWith(".py")
     });
-    console.log(response);
-    console.log(fileName)
+
+    // Now generate the file.
+
+    if(response.lib === 'djs') {
+        console.log("Generating DiscordJS project");
+        let mkdir = utils.promisify(fs.mkdir);
+        await mkdir(path.join(process.cwd(), 'src'))
+    }
 })();
