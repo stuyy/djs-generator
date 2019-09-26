c848d645904229a8658a4fb231372e382628c123
Anson Foong <afoong@albany.edu>
2019-09-26 02:26:29 -0400

    Removed text prompt for library and replaced it with a select prompt.

--- index.js
+++ index.js
@@ -2,41 +2,55 @@
 const prompts = require('prompts');
 const utils = require('./util/util');
 (async () => {
-    console.log(process.cwd())
-    const response = await prompts({
-      type: 'text',
-      name: 'lib',
-      message: 'Which library are you using?',
-      validate: lib => lib === 'djs' || lib === 'discordjs' || lib === 'discord.js' || lib == 'dpy' || lib === 'discord.py'
-    });
-    const projectName = await prompts({
-        type: 'text',
-        name: 'file',
-        message: 'Enter the name of the project'
-    });
-    const botToken = await prompts({
-        type: 'text',
-        name: 'token',
-        message: "Please enter your bot token"
-    });
+    process.argv.shift();
+    process.argv.shift();
+    let args = process.argv;
+    if(args.length === 0) {
+        const projectName = await prompts({
+            type: 'text',
+            name: 'file',
+            message: 'Enter the name of the project',
+            validate: async input => await utils.exists(input) ? true : 'Project name exists already'
+        });
+        const library = await prompts({
+            type: 'select',
+            name: 'value',
+            message: 'Which library are you using?',
+            choices: [
+              { title: 'Discord.JS', description: 'NodeJS library for interacting with the Discord API', value: 'djs' },
+              { title: 'discord.py', description: 'Python library for interacting with the Discord API', value: 'dpy'}
+            ],
+            initial: 1
+        });
+        const confirm = await prompts({
+            type: 'toggle',
+            name: 'value',
+            message: response.lib === 'djs' ? 'Install Commando?' : 'Install Commands Extension?',
+            initial: true,
+            active: 'yes',
+            inactive: 'no'
+        });
+        const framework = await prompts({
+            type: 'text',
+            name: 'choice',
+            message: `Do you want to use the commands framework for ${response.lib}? (Y/N)`,
+            validate: input => input.toLowerCase() === 'y' || input.toLowerCase() === 'yes' || input.toLowerCase() === 'n' || input.toLowerCase() === 'no'
+        })
+        const botToken = await prompts({
+            type: 'invisible',
+            name: 'token',
+            message: "Please enter your bot token"
+        });
+        console.log(confirm.value);
+        await utils.generateProject(response.lib, projectName.file, framework.choice, {
+            "token" : botToken.token
+        });
+        
+    }
+    else if(args.length === 1) {
 
-    utils.generateProject(response.lib, projectName.file, {
-        "token" : botToken.token
-    });
+    }
     
-    // Now generate the file.
+    
 
-    // if(response.lib === 'djs') {
-    //     console.log("Generating DiscordJS project");
-    //     let mkdir = utils.promisify(fs.mkdir);
-    //     await mkdir(path.join(process.cwd(), projectName.file))
-    //     await mkdir(path.join(process.cwd(), projectName.file, 'config'));
-    //     let copyFile = utils.promisify(fs.copyFile);
-    //     await copyFile(path.join(__dirname, 'templates', 'bot.js'), path.join(process.cwd(), projectName.file, 'bot.js'));
-    //     let configObj = {
-    //         "token" : botToken.token
-    //     }
-    //     let writeFile = utils.promisify(fs.writeFile);
-    //     await writeFile(path.join(process.cwd(), projectName.file, 'config', 'config.json'), JSON.stringify(configObj));
-    // }
 })();
