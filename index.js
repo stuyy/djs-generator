eed3257997ee75ae5b7f2e6c79add275f7f4c04c
Anson Foong <afoong@albany.edu>
2019-09-26 02:43:01 -0400

    Moved all questions to an array of object prompts.

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 #!/usr/bin/env node
 const prompts = require('prompts');
 const utils = require('./util/util');
+const constants = require('./util/Constants');
+
+const questions = [
+    {
+        type: 'text',
+        name: 'file',
+        message: 'Enter the name of the project',
+        validate: async input => await utils.exists(input) ? true : 'Project name exists already'
+    },
+    {
+        type: 'select',
+        name: 'lib',
+        message: 'Which library are you using?',
+        choices: [
+          { title: 'Discord.JS', description: 'NodeJS library for interacting with the Discord API', value: 'djs' },
+          { title: 'discord.py', description: 'Python library for interacting with the Discord API', value: 'dpy'}
+        ],
+        initial: 0
+    },
+    {
+        type: 'toggle',
+        name: 'framework',
+        message: prev => prev === 'djs' ? 'Install the Commando Framework?' : 'Install commands Extension?',
+        initial: true,
+        active: 'yes',
+        inactive: 'no'
+    },
+    {
+        type: 'invisible',
+        name: 'token',
+        message: "Please enter your bot token"
+    }
+
+];
+
 (async () => {
     process.argv.shift();
     process.argv.shift();
     let args = process.argv;
     if(args.length === 0) {
-        const projectName = await prompts({
-            type: 'text',
-            name: 'file',
-            message: 'Enter the name of the project',
-            validate: async input => await utils.exists(input) ? true : 'Project name exists already'
-        });
-        const library = await prompts({
-            type: 'select',
-            name: 'value',
-            message: 'Which library are you using?',
-            choices: [
-              { title: 'Discord.JS', description: 'NodeJS library for interacting with the Discord API', value: 'djs' },
-              { title: 'discord.py', description: 'Python library for interacting with the Discord API', value: 'dpy'}
-            ],
-            initial: 0
-        });
-
-        const confirm = await prompts({
-            type: 'toggle',
-            name: 'value',
-            message: libraru.lib === 'djs' ? 'Install the Commando Framework?' : 'Install commands Extension?',
-            initial: true,
-            active: 'yes',
-            inactive: 'no'
-        });
-        const botToken = await prompts({
-            type: 'invisible',
-            name: 'token',
-            message: "Please enter your bot token"
-        });
-        console.log(confirm.value);
-        await utils.generateProject(response.lib, projectName.file, framework.choice, {
-            "token" : botToken.token
+        const res = await prompts(questions);
+        await utils.generateProject(res.lib, res.file, res.framework, {
+            "token" : res.token
         });
         
     }
     else if(args.length === 1) {
-
+        console.log("Yay")
     }
-    
-    
 
 })();
