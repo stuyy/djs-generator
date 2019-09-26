3812248119c97709c08caafb1f00436cf7a07a16
Anson Foong <afoong@albany.edu>
2019-09-26 02:08:31 -0400

    Use a ternary operator to determine whether to generate commando template or bot template.

--- util/util.js
+++ util/util.js
@@ -19,14 +19,13 @@ module.exports.generateProject = async function(type, projectName, framework, co
         fs.mkdir(path.join(CURRENT_DIR, projectName))
         .then(() => fs.mkdir(path.join(CURRENT_DIR, projectName, 'config'))) // Create Config Folder.
         .then(() => fs.copyFile(path.join(__dirname, '..', 'templates', 'registry.js'), path.join(CURRENT_DIR, projectName, 'config', 'registry.js')))
-        .then(() => fs.copyFile(path.join(__dirname, '..', 'templates', 'commando.js'), path.join(CURRENT_DIR, projectName, 'bot.js')))
+        .then(() => framework === 'y' || framework === 'yes' ? fs.copyFile(path.join(__dirname, '..', 'templates', 'commando.js'), path.join(CURRENT_DIR, projectName, 'bot.js')) : fs.copyFile(path.join(__dirname, '..', 'templates', 'bot.js'), path.join(CURRENT_DIR, projectName, 'bot.js')))
         .then(() => fs.writeFile(path.join(CURRENT_DIR, projectName, 'config', 'config.json'), JSON.stringify(configObj, null, 4)))
         .then(() => fs.writeFile(path.join(CURRENT_DIR, projectName, 'djs.json'), JSON.stringify(opts, null, 4)))
         .then(() => fs.readFile(path.join(CURRENT_DIR, projectName, 'djs.json')))
         .then(() => fs.mkdir(path.join(CURRENT_DIR, projectName, 'commands')))
         .then(() => console.log("Generated Discord.JS Project"))
         .catch(err => console.log(err));
-
     }
 }
 
