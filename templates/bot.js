51ecf433cd67041732d10951c3653923f90bce8d
Anson Foong <afoong@albany.edu>
2019-09-27 06:43:23 -0400

    Removed all of the requires in bot.js and moved them to registry.js

--- templates/bot.js
+++ templates/bot.js
@@ -1,6 +1,3 @@
 const discord = require('discord.js');
 const client = new discord.Client({});
-const path = require('path');
-const config = require(path.join(__dirname, 'config', 'config.json'));
-const registry = require(path.join(__dirname, 'config', 'registry'))(client);
-client.login(config.token);
+const registry = require('./config/registry')(client);
