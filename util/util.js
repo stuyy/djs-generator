5ceaa5255029c26cb3aeaac8b324809759621b8d
Anson Foong <afoong@albany.edu>
2019-09-27 06:24:52 -0400

    Moved copyFile function calls on fs to copyTemplates function.

--- util/util.js
+++ util/util.js
@@ -12,16 +12,27 @@ module.exports.generateProject = function(type, projectName, usingFramework, con
         let opts = usingFramework ? { project: projectName, framework: true, groups: [] } : { project: projectName, framework: false }
         fs.mkdir(path.join(CURRENT_DIR, projectName))
         .then(() => fs.mkdir(path.join(CURRENT_DIR, projectName, 'config'))) // Create Config Folder.
-        .then(() => usingFramework ? fs.copyFile(path.join(__dirname, '..', 'templates', 'registry.js'), path.join(CURRENT_DIR, projectName, 'config', 'registry.js')) : Promise.resolve())
-        .then(() => usingFramework ? fs.copyFile(path.join(__dirname, '..', 'templates', 'commando.js'), path.join(CURRENT_DIR, projectName, 'bot.js')) : fs.copyFile(path.join(__dirname, '..', 'templates', 'bot.js'), path.join(CURRENT_DIR, projectName, 'bot.js')))
         .then(() => fs.writeFile(path.join(CURRENT_DIR, projectName, 'config', 'config.json'), JSON.stringify(configObj, null, 4)))
         .then(() => fs.writeFile(path.join(CURRENT_DIR, projectName, 'djs.json'), JSON.stringify(opts, null, 4)))
         .then(() => fs.mkdir(path.join(CURRENT_DIR, projectName, 'commands'))) // Generate Commands Folder for both Scaffolds.
         .then(() =>  fs.mkdir(path.join(CURRENT_DIR, projectName, 'events'))) // Generate Events Folder for both Scaffolds.
-        .then(() => fs.copyFile(path.join(__dirname, '..', 'templates', 'ready.js'), path.join(CURRENT_DIR, projectName, 'events', 'ready.js')))
-        .then(() => fs.copyFile(path.join(__dirname, '..', 'templates', 'message.js'), path.join(CURRENT_DIR, projectName, 'events', 'message.js')))
+        .then(() => copyTemplates({ usingFramework: usingFramework, projectName: projectName}))
         .then(() => console.log("Generated Discord.JS Project"))
         .catch(err => console.log(err));
+    }
+}
+
+async function copyTemplates(opts) {
+
+    try {
+        await fs.copyFile(path.join(__dirname, '..', 'templates', 'ready.js'), path.join(CURRENT_DIR, opts.projectName, 'events', 'ready.js'));
+        await fs.copyFile(path.join(__dirname, '..', 'templates', 'message.js'), path.join(CURRENT_DIR, opts.projectName, 'events', 'message.js'));
+        opts.usingFramework ? await fs.copyFile(path.join(__dirname, '..', 'templates', 'registry.js'), path.join(CURRENT_DIR, opts.projectName, 'config', 'registry.js')) : true
+        opts.usingFramework ? fs.copyFile(path.join(__dirname, '..', 'templates', 'commando.js'), path.join(CURRENT_DIR, opts.projectName, 'bot.js')) : fs.copyFile(path.join(__dirname, '..', 'templates', 'bot.js'), path.join(CURRENT_DIR, opts.projectName, 'bot.js'));
+        console.log("Copied templates...");
+    }
+    catch(err) {
+        console.log(err);
     }
 }
 
