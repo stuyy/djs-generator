099b9aae72571ac81f52efbe23f54279f9a96716
Anson Foong <afoong@albany.edu>
2019-09-27 07:06:35 -0400

    Simple test command template.

---
+++ templates/sample_commands/test.js
@@ -0,0 +1,6 @@
+/**
+ * This is just a test command. You can delete it from the directory.
+ */
+module.exports.run = (client, message, args) => {
+    message.channel.send("Test command works!");
+}
