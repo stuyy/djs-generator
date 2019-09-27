ad5b74b2af611eada5983326f9b818dc2699c880
Anson Foong <afoong@albany.edu>
2019-09-27 06:33:23 -0400

    Removed events from bot.js template

--- templates/bot.js
+++ templates/bot.js
@@ -2,16 +2,5 @@ const discord = require('discord.js');
 const client = new discord.Client({});
 const path = require('path');
 const config = require(path.join(__dirname, 'config', 'config.json'));
-
-client.on('ready', () => {
-    console.log(client.user.username + '#' + client.user.discriminator + ' has logged in.');
-});
-
-client.on('message', message => {
-    if(message.author.bot) return;
-    if(message.content.toLowerCase() === 'ping') {
-        message.reply('pong');
-    }
-});
-
+const registry = require(path.join(__dirname, 'config', 'registry'))(client);
 client.login(config.token);
