2545dc69f81871c7a3051ef4e8ea8f4a42f885bd
Anson Foong <afoong@albany.edu>
2019-09-27 07:02:11 -0400

    Run commands in message.js template

--- templates/message.js
+++ templates/message.js
@@ -1,5 +1,14 @@
+const config = require('../config/config.json');
+
 module.exports = (client, message) => {
     if(message.author.bot) return;
-    if(message.content.toLowerCase() === 'hello')
-        message.reply('Hello!');
+    if(!message.content.startsWith(config.prefix)) return;
+    
+    // Parse Commands
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    const cmd = client.commands.get(command);
+
+    cmd ? cmd.run(client, message, args) : message.channel.send("Command doesn't exist.");
+
 }
