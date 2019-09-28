9e0bcb98b7289592084496c37b8bcee0f21a00a1
Anson Foong <afoong@albany.edu>
2019-09-28 00:54:34 -0400

    Simple file for generating command templates.

---
+++ util/Templates.js
@@ -0,0 +1,22 @@
+module.exports.generateCommand = (options) => {
+    return options.framework ? `
+const commando = require('discord.js-commando');
+module.exports = class ${options.name}Command extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: '${options.name.toLowerCase()}',
+            description: '${options.description}',
+            group: '${options.group.toLowerCase()}',
+            memberName: '${options.memberName.toLowerCase()}'
+        })
+    }
+    async run(msg) {
+        msg.channel.send("${options.name} command works!");
+    }
+}` :
+`
+module.exports.run = (client, message, args) => {
+    message.channel.send('${options.name} command works')
+}
+`;
+}
