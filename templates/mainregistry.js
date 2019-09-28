edbb901cd1a60177e9fed87ca9e45298d09e1bd3
Anson Foong <afoong@albany.edu>
2019-09-28 03:22:38 -0400

    mainregistry will register all commands in the commands folder recursively.

--- templates/mainregistry.js
+++ templates/mainregistry.js
@@ -19,7 +19,7 @@ function registerEvents(client) {
     }))
     .catch(err => console.log(err));
 }
-
+/*
 function registerCommands(client) {
     client.commands = new Map();
     fs.readdir(path.join(__dirname, '..', 'commands'))
@@ -31,4 +31,38 @@ function registerCommands(client) {
         client.commands.set(cmdName, cmdModule);
     }))
     .catch(err => console.log(err));
+} */
+
+function registerCommands(client) { 
+    client.commands = new Map();
+    recurDir(client, 'commands')
+    .then(() => console.log("Good"))
+    .catch(err => console.log(err));
 }
+
+async function recurDir(client, curr) {
+    let dirs = await fs.readdir(path.join(__dirname, '..', curr));
+    if(dirs.length === 0) return; // If current dir is empty, return.
+    else {
+        await dirs.forEachAsync(client, dirs, curr, fs.lstat); // Iterate through each file in the current directory.
+    }
+}
+
+Array.prototype.forEachAsync = async function(client, dirs, currDir, cb) {
+    for (let i = 0; i < dirs.length; i++) {
+        // Call fs.lstat.
+        console.log("CURRENT FILE: " + dirs[i])
+        let curr = await cb(path.join(__dirname, '..', currDir, dirs[i]), i, dirs);
+        // If the file is a directory, call readRecur.
+        if(curr.isDirectory()) 
+            await recurDir(client, path.join(currDir, dirs[i]))
+        else
+        {
+            let cmdName = dirs[i].substring(0, dirs[i].indexOf(".js"));
+            let cmdModule = require(path.join(__dirname, '..', currDir, dirs[i]));
+            console.log(cmdName);
+            console.log(cmdModule);
+            client.commands.set(cmdName, cmdModule);
+        }
+    }
+};
